596d3f42827eb7233ede8b250667804b7c3e6482
Artem Smirnov <webency@mail.ru>
2026-03-28 20:59:13 +0300

    styles

---
+++ src/theme/colors.ts
@@ -0,0 +1,135 @@
+export const baseLight = {
+  primary: '#6852D6',
+  primary50: '#F9F8FD',
+  primary100: '#EDEAFA',
+  primary200: '#DCD7F6',
+  primary300: '#CCC4F1',
+  primary400: '#BBB1ED',
+  primary500: '#AA9EE8',
+  primary600: '#9A8BE4',
+  primary700: '#8978DF',
+  primary800: '#7965DB',
+  primary900: '#5D49BE',
+  neutral50: '#FFFFFF',
+  neutral100: '#FAFAFA',
+  neutral200: '#F5F5F5',
+  neutral300: '#E8E8E8',
+  neutral400: '#DCDCDC',
+  neutral500: '#A1A1A1',
+  neutral600: '#727272',
+  neutral700: '#5B5B5B',
+  neutral800: '#434343',
+  neutral900: '#141414',
+  info: '#0B7BEA',
+  warning: '#FFAB00',
+  success1: '#09C26F',
+  success2: '#079B59',
+  success3: '#3ACE8C',
+  success4: '#56E8A7',
+
+  error1: '#F44649',
+  error2: '#C3383A',
+  error3: '#F66B6D',
+};
+
+export const baseDark = {
+  primary: '#6852D6',
+  primary50: '#15102B',
+  primary100: '#1D173C',
+  primary200: '#251E4D',
+  primary300: '#2E245E',
+  primary400: '#362B6F',
+  primary500: '#3E3180',
+  primary600: '#473892',
+  primary700: '#4F3EA3',
+  primary800: '#5745B4',
+  primary900: '#7460D9',
+  neutral50: '#141414',
+  neutral100: '#1A1A1A',
+  neutral200: '#272727',
+  neutral300: '#383838',
+  neutral400: '#4C4C4C',
+  neutral500: '#858585',
+  neutral600: '#989898',
+  neutral700: '#A8A8A8',
+  neutral800: '#C8C8C8',
+  neutral900: '#FFFFFF',
+  info: '#0D66BF',
+  warning: '#D08D04',
+  success1: '#0B9F5D',
+  success2: '#097F4A',
+  success3: '#3CB27D',
+  success4: '#56E8A7',
+  error1: '#C73C3E',
+  error2: '#9F3032',
+  error3: '#D26365',
+};
+
+const staticColor = {
+  black: '#141414',
+  white: '#ffffff',
+};
+
+const backgroundLight = {
+  bg01: baseLight.neutral50,
+  bg02: baseLight.neutral100,
+  bg03: baseLight.neutral200,
+  bg04: baseLight.neutral300,
+};
+
+const backgroundDark = {
+  bg01: baseDark.neutral50,
+  bg02: baseDark.neutral100,
+  bg03: baseDark.neutral200,
+  bg04: baseDark.neutral300,
+};
+
+const borderLight = {
+  light: baseLight.neutral200,
+  default: baseLight.neutral300,
+  dark: baseLight.neutral400,
+  highlight: baseLight.primary,
+};
+
+const borderDark = {
+  light: baseDark.neutral200,
+  default: baseDark.neutral300,
+  dark: baseDark.neutral400,
+  highlight: baseDark.primary,
+};
+
+const textLight = {
+  primary: baseLight.neutral900,
+  secondary: baseLight.neutral600,
+  tertiary: baseLight.neutral500,
+  disabled: baseLight.neutral400,
+  white: baseLight.neutral50,
+  highlight: baseLight.primary,
+}
+
+const textDark = {
+  primary: baseDark.neutral900,
+  secondary: baseDark.neutral600,
+  tertiary: baseDark.neutral500,
+  disabled: baseDark.neutral400,
+  white: baseDark.neutral50,
+  highlight: baseDark.primary,
+};
+
+const iconLight = {
+  primary: baseLight.neutral900,
+  secondary: baseLight.neutral500,
+  tertiary: baseLight.neutral400,
+  white: baseLight.neutral50,
+  highlight: baseLight.primary,
+};
+
+const iconDark = {
+  primary: baseLight.neutral900,
+  secondary: baseLight.neutral500,
+  tertiary: baseLight.neutral400,
+  white: baseLight.neutral50,
+  highlight: baseLight.primary,
+};
+
+
