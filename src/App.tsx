555831ae998d01c76ea256591e16bfa2ca047930
Artem Smirnov <webency@mail.ru>
2026-03-28 18:18:54 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <AppContent />
+    </SafeAreaProvider>
+  );
+}
+
+function AppContent() {
+
+  return (
+    <View style={styles.container}>
+      <Text>start</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
