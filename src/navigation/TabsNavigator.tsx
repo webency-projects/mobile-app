57c8a26fa73b38b5846fef91fa31af7d8e142f17
Artem Smirnov <webency@mail.ru>
2026-03-29 17:47:01 +0300

    add ios android assets and tabs icon

--- src/navigation/TabsNavigator.tsx
+++ src/navigation/TabsNavigator.tsx
@@ -4,8 +4,43 @@ import { ChatsScreen } from '@/screens/chats/ChatsScreen.tsx';
 import { CallsScreen } from '@/screens/calls/CallsScreen.tsx';
 import { UsersScreen } from '@/screens/users/UsersScreen.tsx';
 import { GroupsScreen } from '@/screens/groups/GroupsScreen.tsx';
+import ChatsDefaultIcon from "@/assets/icons/tabs/ChatsDefault.svg"
+import ChatsSelectedIcon from "@/assets/icons/tabs/ChatsSelected.svg"
+import CallsSelectedIcon from "@/assets/icons/tabs/CallsSelected.svg"
+import CallsDefaultIcon from "@/assets/icons/tabs/CallsDefault.svg"
+import UsersSelectedIcon from "@/assets/icons/tabs/UsersSelected.svg"
+import UsersDefaultIcon from "@/assets/icons/tabs/UsersDefault.svg"
+import GroupsSelectedIcon from "@/assets/icons/tabs/GroupsSelected.svg"
+import GroupsDefaultIcon from "@/assets/icons/tabs/GroupsDefault.svg"
 
 const Tabs = createBottomTabNavigator<TabsParamList>();
+
+type TabIconType = { focused: boolean; color: string; size: number };
+
+const ChatsTabIcon = ({ focused, color, size }: TabIconType) =>
+  focused ? (
+    <ChatsSelectedIcon color={color} width={size} height={size} />
+  ) : (
+    <ChatsDefaultIcon color={color} width={size} height={size} />
+  );
+const CallsTabIcon = ({ focused, color, size }: TabIconType) =>
+  focused ? (
+    <CallsSelectedIcon color={color} width={size} height={size} />
+  ) : (
+    <CallsDefaultIcon color={color} width={size} height={size} />
+  );
+const UsersTabIcon = ({ focused, color, size }: TabIconType) =>
+  focused ? (
+    <UsersSelectedIcon color={color} width={size} height={size} />
+  ) : (
+    <UsersDefaultIcon color={color} width={size} height={size} />
+  );
+const GroupsTabIcon = ({ focused, color, size }: TabIconType) =>
+  focused ? (
+    <GroupsSelectedIcon color={color} width={size} height={size} />
+  ) : (
+    <GroupsDefaultIcon color={color} width={size} height={size} />
+  );
 
 export const TabsNavigator = () => {
   return (
@@ -16,6 +51,7 @@ export const TabsNavigator = () => {
         options={{
           tabBarLabel: 'Chats',
           headerTitle: 'Chats',
+          tabBarIcon: ChatsTabIcon,
         }}
       />
       <Tabs.Screen
@@ -23,24 +59,27 @@ export const TabsNavigator = () => {
         component={CallsScreen}
         options={{
           tabBarLabel: 'Calls',
-          headerTitle: 'Calls' ,
-      }}
+          headerTitle: 'Calls',
+          tabBarIcon: CallsTabIcon,
+        }}
       />
       <Tabs.Screen
         name={'UsersTab'}
         component={UsersScreen}
         options={{
           tabBarLabel: 'Users',
-          headerTitle: 'Users' ,
-      }}
+          headerTitle: 'Users',
+          tabBarIcon: UsersTabIcon,
+        }}
       />
       <Tabs.Screen
         name={'GroupsTab'}
         component={GroupsScreen}
         options={{
           tabBarLabel: 'Groups',
-          headerTitle: 'Groups' ,
-      }}
+          headerTitle: 'Groups',
+          tabBarIcon: GroupsTabIcon,
+        }}
       />
     </Tabs.Navigator>
   );
