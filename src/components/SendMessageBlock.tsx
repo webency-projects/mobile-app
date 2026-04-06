1d513f8d2e5b9d29f86dd4784e03d4f69121f71c
Artem Smirnov <webency@mail.ru>
2026-04-06 18:30:17 +0300

    add textinput into send message

--- src/components/SendMessageBlock.tsx
+++ src/components/SendMessageBlock.tsx
@@ -1,35 +1,45 @@
-import { Text, View, StyleSheet} from "react-native";
-import  SendButton  from "@/assets/icons/base/SendButton.svg"
-import AddIcon from "@/assets/icons/base/plus.circle.no.svg"
-import MicrophoneIcon from "@/assets/icons/base/microphone.no.svg"
-import SmileIcon from "@/assets/icons/base/face.smiling.no.svg"
-import StickerIcon from "@/assets/icons/base/sticker.no.svg"
-import AiIcon from "@/assets/icons/base/ai.no.svg"
+import { View, StyleSheet, TextInput } from 'react-native';
+import SendButton from '@/assets/icons/base/SendButton.svg';
+import AddIcon from '@/assets/icons/base/plus.circle.no.svg';
+import MicrophoneIcon from '@/assets/icons/base/microphone.no.svg';
+import SmileIcon from '@/assets/icons/base/face.smiling.no.svg';
+import StickerIcon from '@/assets/icons/base/sticker.no.svg';
+import AiIcon from '@/assets/icons/base/ai.no.svg';
 
 import { useAppTheme } from '@/hooks/useAppTheme.ts';
+import { useState } from 'react';
 interface IProps {}
 
 export const SendMessageBlock = (props: IProps) => {
-    const {} = props;
-    const {colors} = useAppTheme()
-    return (
-      <View style={[styles.container, {backgroundColor: colors.neutral50}]}>
-        <View style={styles.messageBox}>
-          <Text style={{color: colors.textPrimary}}>Type your message</Text>
-        </View>
-        <View style={[styles.controls, {borderColor: colors.neutral300}]}>
-          <View style={styles.leftControls}>
-            <AddIcon color={colors.iconPrimary}/>
-            <MicrophoneIcon color={colors.iconPrimary}/>
-            <SmileIcon color={colors.iconPrimary}/>
-            <StickerIcon color={colors.iconPrimary}/>
-            <AiIcon color={colors.iconPrimary}/>
-          </View>
-          <SendButton color={colors.neutral300} />
-        </View>
+  const {} = props;
+  const { colors } = useAppTheme();
+  const [message, setMessage] = useState('');
+  const hasMessage = message.trim().length > 0;
+
+  return (
+    <View style={[styles.container, { backgroundColor: colors.neutral50 }]}>
+      <View style={styles.messageBox}>
+        <TextInput
+          style={[styles.input, { borderColor: colors.borderLight }]}
+          value={message}
+          onChangeText={v => setMessage(v)}
+          placeholder={'Type a message'}
+          multiline
+        />
       </View>
-    );
-}
+      <View style={[styles.controls, { borderColor: colors.neutral300 }]}>
+        <View style={styles.leftControls}>
+          <AddIcon color={colors.iconPrimary} />
+          <MicrophoneIcon color={colors.iconPrimary} />
+          <SmileIcon color={colors.iconPrimary} />
+          <StickerIcon color={colors.iconPrimary} />
+          <AiIcon color={colors.iconPrimary} />
+        </View>
+        <SendButton color={hasMessage ? colors.primary : colors.neutral300} />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -48,6 +58,13 @@ const styles = StyleSheet.create({
     gap: 8,
   },
   messageBox: {
+    padding: 8,
+  },
+  input: {
+    minHeight: 30,
+    maxHeight: 120,
+    borderWidth: 1,
+    borderRadius: 8,
     padding: 8,
   },
 });
